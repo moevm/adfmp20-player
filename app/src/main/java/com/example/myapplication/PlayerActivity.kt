6df3d605a912dbe2d0f2a53440efd051e2f72ce6
Alina Gorbunova <alinagorbunova98@mail.ru>
2020-04-15 22:12:35 +0300

    изменение плеера, добавление кликов

--- app/src/main/java/com/example/myapplication/PlayerActivity.kt
+++ app/src/main/java/com/example/myapplication/PlayerActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
@@ -11,6 +13,43 @@ class PlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
+
+        buttonActive.setOnClickListener{
+            Log.v("TEST","ClickButtonActive");
+
+        }
+
+        buttonPrev.setOnClickListener{
+            Log.v("TEST","ClickButtonPrev");
+        }
+
+        buttonNext.setOnClickListener{
+            Log.v("TEST","ClickButtonNext")
+        }
+
+        blockClosePlayer.setOnClickListener{
+            Log.v("TEST","ClickBlockClosePlayer")
+        }
+
+        durationTrack.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    Log.v("TEST", "Change progress")
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    Log.v("TEST", "Touch Start")
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    Log.v("TEST", "Touch End ")
+                }
+            }
+        )
     }
 
 }
